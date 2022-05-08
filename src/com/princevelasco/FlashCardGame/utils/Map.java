8ea6595be0a12ae72c9a5a80a967d026fc174064
Prince Carl Velasco <princevelasco16@gmail.com>
2022-05-08 19:43:12 +0800

    chore: add javadoc

--- src/com/princevelasco/FlashCardGame/utils/Map.java
+++ src/com/princevelasco/FlashCardGame/utils/Map.java
@@ -4,6 +4,13 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Map {
+    /**
+     * Get the key based on the value
+     *
+     * @param map   The map to search for the key.
+     * @param value The value to search for.
+     * @return The key if found, null otherwise.
+     */
     public static String getKeysByValue(java.util.Map<String, String> map, String value) {
         return map.entrySet()
                 .stream()
